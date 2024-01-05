4e5c2d1eb345c7df95dc0c8e6639825ee9b6f154
Anitha <Anitha@LAPTOP-UM4DSUAN>
2024-01-05 13:14:33 +0530

    First Commit

---
+++ Actitime/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,16 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+
+	@Test
+	public void Customer() {
+		
+		Reporter.log("createCustomer",true);
+
+
+	}
+
+}
